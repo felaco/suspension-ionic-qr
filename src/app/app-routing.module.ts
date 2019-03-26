f68975dcebdc8c3fe3423e48b6a868e148bdc100
Yeremy Toro <ytoro@MacBook-Pro-de-Walter.local>
2019-03-26 12:32:27 -0300

    modificacion de instalacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,20 +5,18 @@ const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', loadChildren: './home/home.module#HomePageModule'},
     {path: 'qr-dialog', loadChildren: './page/qr-dialog/qr-dialog.module#QrDialogPageModule'},
-<<<<<<< HEAD
-    {path: 'qr-page', loadChildren: './page/qr-page/qr-page.module#QrPagePageModule'},
-  { path: 'removal', loadChildren: './page/removal/removal.module#RemovalPageModule' },
-  { path: 'installation', loadChildren: './page/installation/installation.module#InstallationPageModule' },
-=======
+
+    {path: 'installation', loadChildren: './page/installation/installation.module#InstallationPageModule' },
+
     {path: 'removal', loadChildren: './page/removal/removal.module#RemovalPageModule'},
     {
-        path: 'material-detail/:index',
+    path: 'material-detail/:index',
         loadChildren: './page/material-detail/material-detail.module#MaterialDetailPageModule'
     },
     {path: 'index-list', loadChildren: './page/index-list/index-list.module#IndexListPageModule'},
     {path: 'actions-page', loadChildren: './page/actions-page/actions-page.module#ActionsPagePageModule'},
-    {path: 'actions-page-redirect', redirectTo:'actions-page'}
->>>>>>> bcb0d9a6eaa0994733e2465f94ad3c39727e530f
+    {path: 'actions-page-redirect', redirectTo: 'actions-page'}
+
 ];
 
 @NgModule({
--- src/app/page/installation/installation.module.ts
+++ src/app/page/installation/installation.module.ts
@@ -6,7 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import {InstallationPage} from './installation.page';
-import {ComponentsModule} from "../../components/components.module";
+import {ComponentsModule} from '../../components/components.module';
 
 const routes: Routes = [
   {
@@ -20,7 +20,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    ComponentsModule
   ],
   declarations: [InstallationPage]
 })
